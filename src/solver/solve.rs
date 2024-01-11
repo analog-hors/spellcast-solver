46beac44c7b872890593cf031f68ef08c7d505a8
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-01-11 13:45:07 +1000

    Rename game to board in solver

--- src/solver/solve.rs
+++ src/solver/solve.rs
@@ -53,14 +53,14 @@ impl PathState {
     }
 }
 
-pub fn solve(game: &Board, trie: &TrieNode, swaps: u8, max_solutions: usize) -> Vec<Solution> {
+pub fn solve(board: &Board, trie: &TrieNode, swaps: u8, max_solutions: usize) -> Vec<Solution> {
     let mut solutions = BinaryHeap::new();
-    search(game, &mut solutions, max_solutions, &mut PathState::default(), trie, swaps);
+    search(board, &mut solutions, max_solutions, &mut PathState::default(), trie, swaps);
     solutions.into_sorted_vec().into_iter().map(|Reverse(s)| s).collect()
 }
 
 fn search(
-    game: &Board,
+    board: &Board,
     solutions: &mut BinaryHeap<Reverse<Solution>>,
     max_solutions: usize,
     current: &mut PathState,
@@ -74,7 +74,7 @@ fn search(
     if trie.is_end_of_word() {
         solutions.push(Reverse(Solution {
             path: current.path.clone(),
-            score: score(game, &current.path),
+            score: score(board, &current.path),
         }));
         if solutions.len() > max_solutions {
             solutions.pop();
@@ -82,31 +82,31 @@ fn search(
     }
 
     for next in current.neighbours() {
-        let existing = game.grid[next as usize];
+        let existing = board.grid[next as usize];
         if let Some(trie) = trie.child(existing) {
             current.push(next, existing);
-            search(game, solutions, max_solutions, current, trie, swaps);
+            search(board, solutions, max_solutions, current, trie, swaps);
             current.pop();
         }
         if swaps > 0 {
             for (letter, trie) in trie.children().filter(|&(l, _)| l != existing) {
                 current.push(next, letter);
-                search(game, solutions, max_solutions, current, trie, swaps - 1);
+                search(board, solutions, max_solutions, current, trie, swaps - 1);
                 current.pop();
             }
         }
     }
 }
 
-fn score(game: &Board, path: &[(Tile, Letter)]) -> u16 {
+fn score(board: &Board, path: &[(Tile, Letter)]) -> u16 {
     let mut word_score = 0;
     let mut word_multiplier = 1;
     for &(tile, letter) in path {
         let mut letter_score = letter_score(letter);
-        if Some(tile) == game.double_letter {
+        if Some(tile) == board.double_letter {
             letter_score *= 2;
         }
-        if Some(tile) == game.double_word {
+        if Some(tile) == board.double_word {
             word_multiplier = 2;
         }
         word_score += letter_score;
